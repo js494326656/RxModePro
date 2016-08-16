d0e90b17dd6d2aee117c2b710a5d369c76f1cea8
jiangshun <494326656@qq.com>
2016-08-16 17:16:38 +0800

    Initial commit

---
+++ rxmode/src/main/java/com/landscape/rxmode/RxMode.java
@@ -0,0 +1,9 @@
+package com.landscape.rxmode;
+
+/**
+ * Created by 1 on 2016/8/15.
+ */
+public class RxMode {
+
+
+}
---
+++ rxmodecomplier/src/main/java/com/landscape/complier/BeanFinderProcesser.java
@@ -0,0 +1,114 @@
+package com.landscape.complier;
+
+import com.google.auto.service.AutoService;
+import com.landscape.annotation.RxBean;
+import com.landscape.model.RxBeanField;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
+import javax.annotation.processing.Messager;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.Elements;
+import javax.tools.Diagnostic;
+
+@AutoService(Processor.class)
+public class BeanFinderProcesser extends AbstractProcessor {
+
+    /**
+     * 使用 Google的auto-service库可以自动生成META-INF/services/javax.annotation.processing.Processor文件
+     */
+
+    private Filer mFiler; //文件相关的辅助类
+    private Elements mElementUtils; //元素相关的辅助类
+    private Messager mMessager; //日志相关的辅助类
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+        mFiler = processingEnv.getFiler();
+        mElementUtils = processingEnv.getElementUtils();
+        mMessager = processingEnv.getMessager();
+    }
+
+    /**
+     * @return 指定哪些注解应该被注解处理器注册
+     */
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        Set<String> types = new LinkedHashSet<>();
+        types.add(RxBean.class.getCanonicalName());
+        return types;
+    }
+
+    /**
+     * @return 指定使用的Java版本。通常返回SourceVersion.latestSupported()。
+     */
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latestSupported();
+    }
+
+    private Map<String, AnnotatedClass> mAnnotatedClassMap = new HashMap<>();
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        mAnnotatedClassMap.clear();
+
+        try {
+            processRxBean(roundEnv);
+        } catch (IllegalArgumentException e) {
+            error(e.getMessage());
+            return true; // stop process
+        }
+
+        for (AnnotatedClass annotatedClass : mAnnotatedClassMap.values()) {
+            try {
+                info("Generating file for %s", annotatedClass.getFullClassName());
+                annotatedClass.generateFinder().writeTo(mFiler);
+            } catch (IOException e) {
+                error("Generate file failed, reason: %s", e.getMessage());
+                return true;
+            }
+        }
+        return true;
+    }
+
+    private void processRxBean(RoundEnvironment roundEnv) throws IllegalArgumentException {
+        for (Element element : roundEnv.getElementsAnnotatedWith(RxBean.class)) {
+            // TODO: 16/8/4 检查字段的修饰符
+            AnnotatedClass annotatedClass = getAnnotatedClass(element);
+            RxBeanField field = new RxBeanField(element);
+            annotatedClass.addField(field);
+        }
+    }
+
+    private AnnotatedClass getAnnotatedClass(Element element) {
+        TypeElement classElement = (TypeElement) element.getEnclosingElement();
+        String fullClassName = classElement.getQualifiedName().toString();
+        AnnotatedClass annotatedClass = mAnnotatedClassMap.get(fullClassName);
+        if (annotatedClass == null) {
+            annotatedClass = new AnnotatedClass(classElement, mElementUtils);
+            mAnnotatedClassMap.put(fullClassName, annotatedClass);
+        }
+        return annotatedClass;
+    }
+
+    private void error(String msg, Object... args) {
+        mMessager.printMessage(Diagnostic.Kind.ERROR, String.format(msg, args));
+    }
+
+    private void info(String msg, Object... args) {
+        mMessager.printMessage(Diagnostic.Kind.NOTE, String.format(msg, args));
+    }
+}
