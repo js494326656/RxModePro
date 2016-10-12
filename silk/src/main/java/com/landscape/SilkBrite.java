81abeb2f54943c7a15b4a0038d212f0bf20ae18c
jiangshun <494326656@qq.com>
2016-10-12 16:28:57 +0800

    11

--- silk/src/main/java/com/landscape/SilkBrite.java
+++ silk/src/main/java/com/landscape/SilkBrite.java
@@ -3,6 +3,7 @@ package com.landscape;
 import android.content.ContentValues;
 import android.support.annotation.CheckResult;
 import android.support.annotation.NonNull;
+import android.text.TextUtils;
 
 import com.orhanobut.logger.AndroidLogTool;
 import com.orhanobut.logger.Logger;
@@ -66,7 +67,7 @@ public class SilkBrite<T> {
     }
 
     private final PublishSubject<T> triggers = PublishSubject.create();
-    private final PublishSubject<Map> nodeTrigger = PublishSubject.create();
+    private final PublishSubject<SilkMsg> nodeTrigger = PublishSubject.create();
 
     private final Scheduler scheduler = Schedulers.io();
 
@@ -93,7 +94,7 @@ public class SilkBrite<T> {
                 .map(new Func1<T, T>() {
                     @Override
                     public T call(T t) {
-                        return SilkBrite.this.silkBeanDriver.getSilkBean();
+                        return silkBeanDriver.getSilkBean();
                     }
                 })
                 .filter(new Func1<T, Boolean>() {
@@ -125,44 +126,39 @@ public class SilkBrite<T> {
 
     public <N> Observable<N> asNodeObservable(final String nodeName) {
         final String[] nodes = nodeName.split("::");
-        Observable<N> nodeObservable = triggers
-                .startWith(silkBeanDriver.getSilkBean())
+        List<String> nodeList = new ArrayList<>(Arrays.asList(nodes));
+        Observable<N> nodeObservable = nodeTrigger
+                .startWith(new SilkMsg(nodeName,requestField(nodeList,silkBeanDriver.getSilkBean())))
                 .subscribeOn(scheduler)
                 .onBackpressureLatest()
-                .map(new Func1<T, T>() {
+                .filter(new Func1<SilkMsg, Boolean>() {
                     @Override
-                    public T call(T t) {
-                        return SilkBrite.this.silkBeanDriver.getSilkBean();
-                    }
-                })
-                .map(new Func1<T, Map>() {
-                    @Override
-                    public Map call(T t) {
-                        Map values = null;
-                        List<String> nodeList = new ArrayList<>(Arrays.asList(nodes));
-                        try {
-                            Object object = SilkBrite.this.requestField(nodeList, t);
-                            values = new HashMap();
-                            values.put(nodeName, object);
-                        } catch (FieldNotMatchedException e) {
-                            values = null;
+                    public Boolean call(SilkMsg silkMsg) {
+                        String tag = silkMsg.getTag();
+                        if (!TextUtils.isEmpty(tag)) {
+                            if (tag.startsWith("::")) {
+                                tag = tag.substring(2);
+                            }
+                            if (tag.endsWith("::")) {
+                                tag = tag.substring(0, tag.length() - 2);
+                            }
+                            if (tag.equals(nodeName)) {
+                                return true;
+                            }
                         }
-                        return values;
+                        return false;
                     }
                 })
-                .filter(new Func1<Map, Boolean>() {
+                .map(new Func1<SilkMsg, Object>() {
                     @Override
-                    public Boolean call(Map map) {
-                        if (map == null) {
-                            return false;
-                        }
-                        return true;
+                    public Object call(SilkMsg t) {
+                        return t.getSilkObj();
                     }
                 })
-                .map(new Func1<Map, N>() {
+                .map(new Func1<Object, N>() {
                     @Override
-                    public N call(Map map) {
-                        return (N) map.get(nodeName);
+                    public N call(Object obj) {
+                        return (N) obj;
                     }
                 })
                 .filter(new Func1<N, Boolean>() {
@@ -181,13 +177,13 @@ public class SilkBrite<T> {
                             throw new IllegalStateException(
                                     "Cannot subscribe to observable for the bean is null.");
                         }
-                        SilkBrite.this.silkBeanDriver.setTrigger(SilkBrite.this.triggers);
+                        SilkBrite.this.silkBeanDriver.setNodeTrigger(nodeName, SilkBrite.this.nodeTrigger);
                     }
                 });
         return nodeObservable;
     }
 
-    private Object requestField(List<String> nodes, Object object) throws FieldNotMatchedException {
+    private Object requestField(List<String> nodes, Object object) {
         Field[] fields;
         if (object.getClass().getName().contains("$$Subcriber")) {
             fields = object.getClass().getSuperclass().getDeclaredFields();
@@ -202,7 +198,6 @@ public class SilkBrite<T> {
                         return field.get(object);
                     } catch (IllegalAccessException e) {
                         e.printStackTrace();
-                        throw new FieldNotMatchedException();
                     }
                 } else {
                     try {
@@ -211,12 +206,11 @@ public class SilkBrite<T> {
                         return requestField(nodes, field.get(object));
                     } catch (Exception e) {
                         e.printStackTrace();
-                        throw new FieldNotMatchedException();
                     }
                 }
             }
         }
-        throw new FieldNotMatchedException();
+        return object;
     }
 
 }
--- silkcomplier/src/main/java/com/landscape/complier/AnnotatedClass.java
+++ silkcomplier/src/main/java/com/landscape/complier/AnnotatedClass.java
@@ -1,18 +1,14 @@
 package com.landscape.complier;
 
 import com.landscape.model.RxBeanType;
-import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.ParameterizedTypeName;
 import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeSpec;
 
-import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.Modifier;
@@ -44,11 +40,19 @@ public class AnnotatedClass {
     public JavaFile generateFinder() throws Exception {
         List<MethodSpec> injectMethods = new ArrayList<>();
         // method inject(final T host, Object source, Provider provider)
-        MethodSpec.Builder sendSilkMethodBuilder = MethodSpec.methodBuilder("sendTrigger")
+        MethodSpec.Builder sendSilkMethodBuilder = MethodSpec.methodBuilder("sendModeNotify")
                 .addModifiers(Modifier.PUBLIC)
                 .addAnnotation(Override.class)
                 .addParameter(TypeName.get(mClassElement.asType()), "bean")
                 .addStatement("if(silkTrigger != null){silkTrigger.onNext(bean);}");
+
+        // add notifyNode
+        MethodSpec.Builder notifyNodeMethodBuilder = MethodSpec.methodBuilder("notifyNode")
+                .addModifiers(Modifier.PUBLIC)
+                .addAnnotation(Override.class)
+                .addParameter(String.class, "tag")
+                .addParameter(Object.class, "bean")
+                .addStatement("if(nodeTrigger != null){nodeTrigger.onNext(new SilkMsg(preTag+$S+tag,bean));}", "::");
 
         // trigger setter
         MethodSpec.Builder triggerSetterBuilder = MethodSpec.methodBuilder("setSilkTrigger")
@@ -57,23 +61,29 @@ public class AnnotatedClass {
                 .addParameter(TypeUtil.PUBLISHSUBJECT, "silkTrigger")
                 .addStatement("this.silkTrigger = silkTrigger");
 
+        // trigger setter
+        MethodSpec.Builder nodeTriggerSetterBuilder = MethodSpec.methodBuilder("setNodeTrigger")
+                .addModifiers(Modifier.PUBLIC)
+                .addAnnotation(Override.class)
+                .addParameter(TypeUtil.PUBLISHSUBJECT, "nodeTrigger")
+                .addStatement("this.nodeTrigger = nodeTrigger");
+
         // add sendtrigger()
         for (int i = 0; i < mType.getMethodElement().size(); i++) {
             ExecutableElement element = mType.getMethodElement().get(i);
             if (element.getSimpleName().toString().startsWith("set")) {
                 List<VariableElement> params = (List<VariableElement>) mType.getMethodElement().get(i).getParameters();
                 if (params.size() > 0) {
-                    String paramName = element.getSimpleName().toString().replace("set", "");
+                    String getterName = element.getSimpleName().toString().replace("set", "");
                     boolean find = false;
                     for (int j = 0; j < mType.getMethodElement().size(); j++) {
                         ExecutableElement element1 = mType.getMethodElement().get(j);
-                        System.out.println("scan:" + element.getSimpleName());
                         if ((element1.getSimpleName().toString().startsWith("is") ||
                                 element1.getSimpleName().toString().startsWith("get")) &&
                                 element1.getSimpleName().toString()
                                         .replace("is", "")
-                                        .replace("get", "").equals(paramName)) {
-                            paramName = element1.getSimpleName().toString();
+                                        .replace("get", "").equals(getterName)) {
+                            getterName = element1.getSimpleName().toString();
                             find = true;
                             break;
                         }
@@ -86,34 +96,62 @@ public class AnnotatedClass {
                     if (find) {
                         setterMethod
                                 .addStatement("silkNotify = ($N() != $N || ((Object)$N()).hashCode() != ((Object)$N).hashCode())",
-                                        paramName, params.get(0).getSimpleName().toString(),
-                                        paramName, params.get(0).getSimpleName().toString())
+                                        getterName, params.get(0).getSimpleName().toString(),
+                                        getterName, params.get(0).getSimpleName().toString())
                                 .addStatement("super.$N($N)", element.getSimpleName().toString(), params.get(0).getSimpleName().toString())
-                                .addStatement("if(silkNotify){sendTrigger(this);}");
+                                .addStatement("if(silkNotify){notifyNode($S,$N);sendModeNotify(this);}",
+                                        params.get(0).getSimpleName().toString(),
+                                        params.get(0).getSimpleName().toString());
                     } else {
                         setterMethod
                                 .addStatement("super.$N($N)", element.getSimpleName().toString(), params.get(0).getSimpleName().toString())
-                                .addStatement("sendTrigger(this)");
+                                .addStatement("notifyNode($S,$N)",
+                                        params.get(0).getSimpleName().toString(),
+                                        params.get(0).getSimpleName().toString())
+                                .addStatement("sendModeNotify(this)");
                     }
                     injectMethods.add(
                             setterMethod.build());
                 }
             }
         }
+
+        // add setPreTag
+        MethodSpec.Builder setPreTagBuilder = MethodSpec.methodBuilder("setPreTag")
+                .addModifiers(Modifier.PUBLIC)
+                .addAnnotation(Override.class)
+                .addParameter(String.class, "preTag")
+                .addStatement("this.preTag = preTag");
+
+        // add getPreTag
+        MethodSpec.Builder getPreTagBuilder = MethodSpec.methodBuilder("getPreTag")
+                .addModifiers(Modifier.PUBLIC)
+                .addAnnotation(Override.class)
+                .returns(String.class)
+                .addStatement("return preTag");
+
         // generate whole class
         TypeSpec.Builder finderClassBuilder = TypeSpec.classBuilder(mClassElement.getSimpleName() + "$$Subcriber")
                 .superclass(TypeName.get(mClassElement.asType()))
                 .addModifiers(Modifier.PUBLIC)
                 .addSuperinterface(ParameterizedTypeName.get(TypeUtil.SUBCRIBER, TypeName.get(mClassElement.asType())))
                 .addMethod(sendSilkMethodBuilder.build())
+                .addMethod(notifyNodeMethodBuilder.build())
                 .addMethod(triggerSetterBuilder.build())
+                .addMethod(nodeTriggerSetterBuilder.build())
+                .addMethod(setPreTagBuilder.build())
+                .addMethod(getPreTagBuilder.build())
                 .addField(PublishSubject.class, "silkTrigger")
+                .addField(PublishSubject.class, "nodeTrigger")
+                .addField(TypeUtil.SILK_MSG, "silkMsg")//just for import the package,fuck!
+                .addField(String.class, "preTag")
                 .addField(Boolean.class, "silkNotify");
         for (MethodSpec injectMethodSpec : injectMethods) {
             finderClassBuilder.addMethod(injectMethodSpec);
         }
         TypeSpec finderClass = finderClassBuilder.build();
         String packageName = mElementUtils.getPackageOf(mClassElement).getQualifiedName().toString();
-        return JavaFile.builder(packageName, finderClass).build();
+        return JavaFile.builder(packageName, finderClass)
+                .build();
     }
 }
